7eee401a17eec25ae8bae2f01a13162ce9972411
VID\jona655r <levendeslot@gmail.com>
2016-09-29 21:08:02 +0200

    no message

--- Assets/Scripts/Lifemanager.cs
+++ Assets/Scripts/Lifemanager.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class Lifemanager : MonoBehaviour {
+public class Lifemanager : MonoBehaviour
+{
+    public Sprite emptyHeart;
+    public Sprite fullHeart;
 
-	// Use this for initialization
-	void Start () {
-	
+    public GameObject healthParent;
+    //to get the prefab that the image is on
+    public Image heartPrefab;
+
+    List<Image> healthBar = new List<Image>();
+
+	void Start ()
+    {
+        for (int i = 0; i < Player.Instance.maxHP; i++)
+        {
+            Image clone = Instantiate(heartPrefab);
+            clone.transform.SetParent(transform);
+            healthBar.Add(clone);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+    void Update () {
+        
 	}
 }
