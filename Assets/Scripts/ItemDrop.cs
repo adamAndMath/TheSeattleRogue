a81d3db55457edd4036d1a3259ae210aa43de523
Adam <adam.and.math@gmail.com>
2016-10-06 15:29:47 +0200

    Added item drop utility

---
+++ Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public class ItemDrop
+{
+    public GameObject obj;
+    [Range(0, 1)]
+    public float chance;
+
+    public void Drop(Vector2 position)
+    {
+        if (Random.value > chance) return;
+
+        GameObject clone = GameObject.Instantiate(obj);
+        clone.transform.position = position;
+    }
+}
