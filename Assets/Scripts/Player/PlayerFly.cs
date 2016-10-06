d69886bb76fd044fd697f31df9a48b868f9d697e
Bastian <bastianxx04@gmail.com>
2016-10-06 20:09:17 +0200

    added vibration in controller

--- Assets/Scripts/Player/PlayerFly.cs
+++ Assets/Scripts/Player/PlayerFly.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using XInputDotNetPure;
 
 public class PlayerFly : StateMachineBehaviour
 {
@@ -6,15 +7,43 @@ public class PlayerFly : StateMachineBehaviour
     private Vector2 dir;
     private Player player;
 
+    private bool playerIndexSet;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = animator.GetComponent<Player>();
-        dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+            GamePad.SetVibration(playerIndex, Mathf.Clamp01(1.1F - dir.x), Mathf.Clamp01(1.1F + dir.x));
+
     }
 
     public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player.MoveHorizontal(dir.x*speed*Time.deltaTime);
         player.MoveVertical(dir.y*speed*Time.deltaTime);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        GamePad.SetVibration(playerIndex, 0, 0);
     }
 }
