cf538da8dc2b1ee5a783a4ef49f96a18324bd988
Bastian <bastianxx04@gmail.com>
2016-10-17 15:55:01 -0700

    made spawn node

---
+++ Assets/Scripts/SpawnNode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnNode : MonoBehaviour
+{
+    public GameObject[] Spawns = new GameObject[1];
+    private int random;
+
+	void Start ()
+	{
+	    random = Random.Range(0, Spawns.Length);
+	    Instantiate(Spawns[random],transform.position,transform.rotation);
+        Debug.Log(""+random);
+	}
+	
+}
