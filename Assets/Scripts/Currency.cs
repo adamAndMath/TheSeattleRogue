428fa91272a73aeaeed931bea9e61a70bbc7df44
Adam <adam.and.math@gmail.com>
2016-10-17 14:44:37 -0700

    Player can pickup money

---
+++ Assets/Scripts/Currency.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Currency : PhysicsObject
+{
+    public int val;
+    public float gravity;
+    public Vector2 speed;
+
+    void Update()
+    {
+        if (MoveHorizontal(speed.x*Time.deltaTime))
+            speed.x = 0;
+
+        if (MoveVertical((speed.y - gravity*Time.deltaTime/2) * Time.deltaTime))
+            speed = Vector2.zero;
+        else
+            speed.y -= gravity*Time.deltaTime;
+
+        int size = collider2D.Cast(Vector2.down, rayHits, 0);
+
+        for (int i = 0; i < size; i++)
+        {
+            Player player = rayHits[i].collider.GetComponent<Player>();
+            if (player)
+            {
+                player.money += val;
+                Destroy(this);
+            }
+        }
+    }
+}
