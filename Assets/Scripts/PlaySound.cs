90ef616eb8a4e97e91a2fdf260c80429b6d7b3be
Bastian <bastianxx04@gmail.com>
2016-10-20 12:26:06 -0700

    added sounds and made player play said sounds

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    private AudioSource audio;
+    public GameObject AudioPlayer;
+
+    public void Play(AudioClip sound)
+    {
+        GameObject playerObject = Instantiate(AudioPlayer);
+        playerObject.transform.position = transform.position;
+	    audio = playerObject.GetComponent<AudioSource>();
+        audio.clip = sound;
+        audio.Play();
+        Destroy(playerObject,3);
+    }
+}
