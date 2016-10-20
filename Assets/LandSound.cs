48410ceeec5cecb732ab414247e17fe50caa42a0
Bastian <bastianxx04@gmail.com>
2016-10-20 14:40:32 -0700

    fixed invourn frames and attack time

---
+++ Assets/LandSound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandSound : StateMachineBehaviour
+{
+    public AudioClip LandSoundClip;
+	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+	    animator.GetComponent<PlaySound>().Play(LandSoundClip);
+	}
+
+}
