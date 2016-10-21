2a0e237c827b144a66170e71bfb281b4242730bc
Adam <adam.and.math@gmail.com>
2016-10-21 15:27:09 -0700

    Camera movement

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public Vector2 min;
+    public Vector2 max;
+    public Vector2 offset;
+    private float z;
+
+    void Start()
+    {
+        z = transform.position.z;
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = Player.Instance.transform.position;
+        transform.position = new Vector3(Mathf.Clamp(playerPos.x + offset.x, min.x, max.x), Mathf.Clamp(playerPos.y + offset.y, min.y, max.y), z);
+    }
+}
