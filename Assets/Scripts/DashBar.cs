f4cd5fef0904099160c111c3418dff4967331391
Bastian <bastianxx04@gmail.com>
2016-10-25 10:41:02 -0700

    added functionen dash bar with dash cooldown

---
+++ Assets/Scripts/DashBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DashBar : MonoBehaviour
+{
+    private RectTransform BackDrop;
+    public RectTransform BarOfDashing;
+
+	void Start ()
+	{
+	    BackDrop = GetComponent<RectTransform>();
+
+        BackDrop.sizeDelta = new Vector2(Player.Instance.MaxDash, BackDrop.sizeDelta.y);
+    }
+	
+	void Update ()
+    {
+	    BarOfDashing.sizeDelta = new Vector2(Player.Instance.DashPower,BarOfDashing.sizeDelta.y);
+	}
+}
