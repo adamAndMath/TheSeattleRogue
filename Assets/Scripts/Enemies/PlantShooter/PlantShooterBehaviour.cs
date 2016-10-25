c88566dde9ace96f8eaaa7c4d7e85ed48d608f5a
VID\gust0452 <gust0452@videndjurs.net>
2016-10-25 18:36:14 +0200

    Lots of meta files

---
+++ Assets/Scripts/Enemies/PlantShooter/PlantShooterBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantShooterBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+    protected void Start()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
