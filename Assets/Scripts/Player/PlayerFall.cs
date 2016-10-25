8859057be3d57fd0f25823ce6e1ce2c16287268c
Adam <adam.and.math@gmail.com>
2016-10-25 10:14:53 -0700

    Constant Acceleration Wrapper

--- Assets/Scripts/Player/PlayerFall.cs
+++ Assets/Scripts/Player/PlayerFall.cs
@@ -14,9 +14,7 @@ public class PlayerFall : StateMachineBehaviour
 
     public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        float move = (speed + gravity * Time.deltaTime / 2) * Time.deltaTime;
-        speed += gravity * Time.deltaTime;
-
+        float move = PhysicsObject.ConstantAcceleration(gravity, ref speed);
         player.MoveVertical(-move);
     }
 }
