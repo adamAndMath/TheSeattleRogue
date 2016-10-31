66c2a54f178d8e6e49931c9ced5ce658e7b9c617
Adam <adam.and.math@gmail.com>
2016-10-31 13:46:12 -0700

    Simple item asset

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public Sprite sprite;
+    public AnimatorOverrideController animator;
+    public Vector2 collisionOffset;
+    public Vector2 collisionSize;
+    public int damage;
+    public int price;
+}
