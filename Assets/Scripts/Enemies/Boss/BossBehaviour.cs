4f7b4d3f7ddfb695cf9d7432f96349115c8e83b8
VID\gust0452 <gust0452@videndjurs.net>
2016-10-31 21:46:01 +0100

    Minor Changes

--- Assets/Scripts/Enemies/Boss/BossBehaviour.cs
+++ Assets/Scripts/Enemies/Boss/BossBehaviour.cs
@@ -4,7 +4,13 @@ using System.Runtime.InteropServices;
 
 public class BossBehaviour : PhysicsObject
 {
+    float shake = 0;
+    float shakeAmount = 0.7f;
+    float decreaseFactor = 1.0f;
+
     public int Health;
+
+    public Camera cam;
 
     public Animator animator;
 
@@ -13,16 +19,28 @@ public class BossBehaviour : PhysicsObject
 	{
         base.Start();
 	    animator = GetComponent<Animator>();
+	    cam = FindObjectOfType<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-
+        CameraShake();
     }
 
     public void CameraShake()
     {
-        
+        {
+            if (shake >= 0)
+            {
+                cam.transform.localPosition = Random.insideUnitSphere * shakeAmount;
+                shake -= Time.deltaTime * decreaseFactor;
+
+            }
+            else
+            {
+                shake = 0.0f;
+            }
+        }
     }
 }
