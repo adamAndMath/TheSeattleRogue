37312e3780bd0d9c928d1cb6b7175716d7724a46
Bastian <bastianxx04@gmail.com>
2016-10-30 18:14:55 -0700

    added working shop and bank to TestScene_BASTIAN

--- Assets/Scripts/UI/ShopKeeper.cs
+++ Assets/Scripts/UI/ShopKeeper.cs
@@ -4,23 +4,39 @@ using System.Collections;
 public class ShopKeeper : MonoBehaviour
 {
     public GameObject ShopGameObject;
+    public GameObject SpeechBubble;
     public float ShopRange = 4F;
     private bool ShopActive;
 	
 	void Update ()
     {
+	    if (Vector2.Distance(Player.Instance.transform.position, transform.position) <= ShopRange)
+	    {
+	        SpeechBubble.SetActive(true);
+	    }
+	    else
+	    {
+	        SpeechBubble.SetActive(false);
+	    }
+
 	    if (Input.GetKeyDown(KeyCode.W) && Vector2.Distance(Player.Instance.transform.position,transform.position) <=  ShopRange)
 	    {
 	        ShopActive =! ShopActive;
         }
 
-	    if (ShopActive)
+	    if (Input.GetKeyDown(KeyCode.Escape) && ShopActive)
+	    {
+	        ShopActive = false;
+	    }
+
+            if (ShopActive)
 	    {
 	        ShopGameObject.SetActive(true);
 	        Time.timeScale = 0;
 	    }
-	    else if(Input.GetKeyDown(KeyCode.W) && ShopActive)
-	    {            
+	    else if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Escape)) && ShopActive)
+	    {
+            ShopGameObject.SetActive(false);
             Time.timeScale = 1;
         }
 	    else
---
+++ Assets/Scripts/UI/WeaponBuyUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WeaponBuyUI : MonoBehaviour
+{
+    //replace gameobject with the weapon
+    public GameObject[] weapons = new GameObject[1];
+	public GameObject[] WeapnDescription = new GameObject[1];
+    public int[] Prices = new int[1];
+
+    public void ClickedWeapon(int WeaponID)
+    {
+        foreach (GameObject i in WeapnDescription)
+        {
+            i.SetActive(false);
+        }
+            WeapnDescription[WeaponID].SetActive(true);
+    }
+
+    public void ClickedBuy(int WeaponID)
+    {
+        if (Player.Instance.money >= Prices[WeaponID])
+        {
+            //Player.Instance.Weapon = weapons[WeaponID];
+            WeapnDescription[WeaponID].GetComponentInChildren<Button>().interactable = false;
+            Player.Instance.money -= Prices[WeaponID];
+        }
+    }
+}
