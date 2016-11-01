1f22e9983effc0ab40bcfdd7b74fcd84de31d609
Adam <adam.and.math@gmail.com>
2016-10-31 22:24:28 -0700

    - Moved pickup scripts into a folder and made a parent class - Added a health pickup

---
+++ Assets/Scripts/Pickup/Currency.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Currency : Pickup
+{
+    public int val;
+    public AudioSource audioObject;
+    public AudioClip coinSoundAudioClip;
+
+    protected override void PickedUp(Player player)
+    {
+        player.money += val;
+
+        AudioSource clone = Instantiate(audioObject);
+        clone.clip = coinSoundAudioClip;
+        clone.Play();
+        clone.gameObject.hideFlags = HideFlags.HideInHierarchy;
+        Destroy(clone.gameObject, 3);
+    }
+}
