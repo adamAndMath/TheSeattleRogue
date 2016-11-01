564d969e819353485b99e8ace417df9f4529c123
VID\gust0452 <gust0452@videndjurs.net>
2016-11-01 07:01:42 +0100

    Boss Run Finished, but problems with boulders casting null reference

--- Assets/Scripts/Enemies/Boss/BossBehaviour.cs
+++ Assets/Scripts/Enemies/Boss/BossBehaviour.cs
@@ -1,24 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 public class BossBehaviour : PhysicsObject
 {
+    public static BossBehaviour Instance { get; private set; }
+
     public float shakeTime;
     public bool shakeIsReady;
     public float shakeAmount;
+    public float timeBetweenBoulders;
+    public float boulderSpawnRange;
 
     private float remainingShakeTime;
     private float shakeX;
     private float shakeY;
     private float startingShakeAmount;
     private Vector3 startingPos;
+    private float timeLeftBetweenBoulders;
+    private float fallSpeed = -10;
 
     public int Health;
 
     public Camera cam;
-
+    public List<GameObject> boulders;
     public Animator animator;
+    private Collider2D coll;
 
 	// Use this for initialization
 	protected override void Start ()
@@ -29,15 +37,22 @@ public class BossBehaviour : PhysicsObject
 	    remainingShakeTime = shakeTime;
 	    startingShakeAmount = shakeAmount;
 	    startingPos = cam.transform.position;
+	    timeLeftBetweenBoulders = timeBetweenBoulders;
+	    coll = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (shakeTime <= 0)
+        if (coll.IsTouching(Player.Instance.GetComponent<Collider2D>()))
         {
-            shakeIsReady = true;
-        }   
+            Player.Instance.Damaged(1);
+        }
+
+        if (!IsGrounded())
+        {
+            MoveVertical(fallSpeed * Time.deltaTime);
+        }
     }
     public bool CameraShake()
     {
@@ -60,5 +75,19 @@ public class BossBehaviour : PhysicsObject
         }
 
         return result;
+    }
+
+    public void fallingBoulders()
+    {
+        Debug.Log("Boulders Being Run");
+        if (timeLeftBetweenBoulders <= 0)
+        {
+            Instantiate(boulders[Random.Range(0, boulders.Count)], new Vector2(Random.Range(-boulderSpawnRange, boulderSpawnRange), 10), Quaternion.identity);
+            timeLeftBetweenBoulders = timeBetweenBoulders;
+        }
+        else
+        {
+            timeLeftBetweenBoulders -= Time.deltaTime;
+        }
     }
 }
