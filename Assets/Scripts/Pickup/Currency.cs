f5a4e1ed6939f2cbcdcbd329a6d0f4dd0624eb6c
Bastian <bastianxx04@gmail.com>
2016-11-01 10:19:31 -0700

    coin works with sound, and polished player animation

--- Assets/Scripts/Pickup/Currency.cs
+++ Assets/Scripts/Pickup/Currency.cs
@@ -3,17 +3,14 @@
 public class Currency : Pickup
 {
     public int val;
-    public AudioSource audioObject;
-    public AudioClip coinSoundAudioClip;
+    public GameObject audioObject;
 
     protected override void PickedUp(Player player)
     {
         player.money += val;
 
-        AudioSource clone = Instantiate(audioObject);
-        clone.clip = coinSoundAudioClip;
-        clone.Play();
-        clone.gameObject.hideFlags = HideFlags.HideInHierarchy;
-        Destroy(clone.gameObject, 3);
+        GameObject clone = Instantiate(audioObject);
+        clone.hideFlags = HideFlags.HideInHierarchy;
+        Destroy(clone, 3);
     }
 }
