cfc5943649650f5c0a87c52246b0d0d9aad566b3
VID\gust0452 <gust0452@videndjurs.net>
2016-11-02 03:33:48 +0100

    Fixed Chest And Currency Spawn

--- Assets/Scripts/Enemies/Chest/ChestBehaviour.cs
+++ Assets/Scripts/Enemies/Chest/ChestBehaviour.cs
@@ -1,16 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ChestBehaviour : MonoBehaviour {
+public class ChestBehaviour : Enemy
+{
+    public GameObject orbs;
+    public float spitSpeed;
 
+    private Collider2D coll;
+    private float timer;
+    private int spitAmount;
+    private bool isTriggered;
+    
 	// Use this for initialization
-    void Start () 
+    void Start ()
     {
-	}
+        coll = GetComponent<Collider2D>();
+        spitAmount = Random.Range(7, 11);
+    }
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    
+	    if (coll.IsTouching(Player.Instance.GetComponent<Collider2D>()))
+	    {
+	        isTriggered = true;
+	    }
+
+	    if (isTriggered)
+	    {
+            if (spitAmount > 0 && timer <= 0)
+            {
+                Instantiate(orbs, transform.position, Quaternion.identity);
+                spitAmount--;
+                timer = spitSpeed;
+            }
+	        timer -= Time.deltaTime;
+	    }
 	}
 }
