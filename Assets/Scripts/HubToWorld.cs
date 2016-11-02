2a834ab9cc1e8623d51b3b6f6e344d40bd2b1d1f
Adam <adam.and.math@gmail.com>
2016-11-01 19:59:50 -0700

    Added transition from hub to world

---
+++ Assets/Scripts/HubToWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubToWorld : MonoBehaviour
+{
+    public int scene;
+    private Collider2D coll;
+
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        if (coll.IsTouching(Player.Instance.GetComponent<Collider2D>()))
+            SceneManager.LoadScene(scene);
+    }
+}
